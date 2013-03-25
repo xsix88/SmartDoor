3f5472256aeb55fdf317b9778732398d569b6721
xsix88 <xsix88@gmail.com>
2013-03-25 17:00:17 +0100

    Empty shell created, admin activity added.

--- src/itu/dk/masterthesis/smartdoor/MainActivity.java
+++ src/itu/dk/masterthesis/smartdoor/MainActivity.java
@@ -1,8 +1,10 @@
 package itu.dk.masterthesis.smartdoor;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -15,7 +17,21 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
+		MenuItem item = menu.add(Menu.NONE, Menu.FIRST, Menu.NONE, "Admin");
+		item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		
 		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onMenuItemSelected(int id, MenuItem item) {
+		// Receipt behavior
+		if(item.getItemId() == (Menu.FIRST)) {
+			Intent intent = new Intent(MainActivity.this, AdminActivity.class);
+			startActivity(intent);
+		}
+		
 		return true;
 	}
 
